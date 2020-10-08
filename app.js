2534f08cae46751f7cafb1391b6367884a8f506c
Bussola <thiagobussola@hotmail.com>
2020-10-08 15:26:48 +0000

    Ajutes finais

--- app.js
+++ app.js
@@ -8,11 +8,30 @@ var server = app.listen(3000, function() {
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 // criando conexão por websocket
 io.on('connection', function(socket){
     console.log('Usuário conectou')
 
     socket.on('disconnect', function() {
         console.log('Usuário desconectou')
+    })
+
+    socket.on('msgParaServidor', function(data){
+
+        //dialogo
+        socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem})
+        
+        socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem})
+
+        //participantes
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+
+            socket.emit('participantesParaCliente', {apelido: data.apelido})
+            
+            socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido})
+        }
+
     })
 })
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,5 @@
+const { emit } = require("../../config/server");
+
 module.exports.iniciaChat = function(application, req, res) {
 
     var dadosForm = req.body;
@@ -11,5 +13,7 @@ module.exports.iniciaChat = function(application, req, res) {
         return;
     }
 
-    res.render('chat')
+    application.get('io').emit('msgParaCliente', {apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'})
+
+    res.render('chat', {dadosForm: dadosForm})
 }
